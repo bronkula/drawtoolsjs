e9de7b14908120c67251cac7ff3cef95be2ffa1d
Hamilton <hamdiggy@gmail.com>
2016-08-29 20:01:59 -0700

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,11 +1,11 @@
 
 /* This function takes two objects, and replaces or adds any values in object 1 with the values of object 2 */
 function overRide(op,ov) {
-    if(!ov) return op;
-    for(var i in ov) {
-        op[i] = ov[i];
-    }
-    return op;
+   if(!ov) return op;
+   for(var i in ov) {
+      op[i] = ov[i];
+   }
+   return op;
 }
 
 
@@ -16,89 +16,124 @@ function overRide(op,ov) {
 
 /* Draw a circle */
 function drawCircle(ctx,x,y,r,options){
-    ctx.beginPath();
-    ctx.arc(x,y,r,0,2*Math.PI);
-    fillIt(ctx,options);
-    strokeIt(ctx,options);
+   ctx.beginPath();
+   ctx.arc(x,y,r,0,2*Math.PI);
+   fillIt(ctx,options);
+   strokeIt(ctx,options);
 }
 /* Draw a rectangle */
 function drawRect(ctx,x,y,w,h,options){
-    ctx = overRide(ctx,options);
-    ctx.fillRect(x,y,w,h);
+   ctx = overRide(ctx,options);
+   ctx.fillRect(x,y,w,h);
 }
 /* Draw a rectangle with a random color */
 function drawRandomRect(ctx,x,y,w,h,options){
-    o = overRide({
-        lineWidth:0,
-        strokeStyle:"black",
-        fillStyle:"rgba("+rand(120,250)+","+rand(120,250)+","+rand(120,250)+","+(options&&options.opacity!==undefined?options.opacity:0.7)+")"
-    },options);
-    drawRect(ctx,x,y,w,h,o);
+   o = overRide({
+      lineWidth:0,
+      strokeStyle:"black",
+      fillStyle:"rgba("+rand(120,250)+","+rand(120,250)+","+rand(120,250)+","+(options&&options.opacity!==undefined?options.opacity:0.7)+")"
+   },options);
+   drawRect(ctx,x,y,w,h,o);
 }
 /* Draw one line segment */
 function drawSegment(ctx,x1,y1,x2,y2,options){
-    ctx.beginPath();
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2,y2);
-    strokeIt(ctx,options)
+   ctx.beginPath();
+   ctx.moveTo(x1,y1);
+   ctx.lineTo(x2,y2);
+   strokeIt(ctx,options)
 }
 /* Draw an array of line segments, allowing smooth connections */
 function drawLine(ctx,lines,options){
-    ctx.beginPath();
-    ctx.moveTo(lines[0].x,lines[0].y);
-    for(var i=1,l=lines.length; i<l; i++) {
-        ctx.lineTo(lines[i].x,lines[i].y);
-    }
-    strokeIt(ctx,options);
+   ctx.beginPath();
+   ctx.moveTo(lines[0].x,lines[0].y);
+   for(var i=1,l=lines.length; i<l; i++) {
+      ctx.lineTo(lines[i].x,lines[i].y);
+   }
+   strokeIt(ctx,options);
 }
 
 function drawText(ctx,text,x,y,options){
-    o = overRide({
-        strokeStyle:"black",
-        lineWidth:0,
-        radius:10,
-        textAlign:"center",
-        font:"16px Verdana"
-    },options);
+   o = overRide({
+      strokeStyle:"black",
+      lineWidth:0,
+      radius:10,
+      textAlign:"center",
+      font:"16px Verdana"
+   },options);
 
-    ctx = overRide(ctx,o);
+   ctx = overRide(ctx,o);
 
-    ctx.globalCompositeOperation = "destination-out";
-    if(o.lineWidth) ctx.strokeText(text,x,y);
+   ctx.globalCompositeOperation = "destination-out";
+   if(o.lineWidth) ctx.strokeText(text,x,y);
 
-    ctx.globalCompositeOperation = "source-over";
-    ctx.fillText(text,x,y);
+   ctx.globalCompositeOperation = "source-over";
+   ctx.fillText(text,x,y);
 }
 
 
 function drawPulse2(ctx,x,y,oRadius,iRadius,options){
-    drawCircle(ctx,x,y,oRadius,options);
-    ctx.globalCompositeOperation = "destination-out";
-    drawCircle(ctx,x,y,iRadius,options);
-    ctx.globalCompositeOperation = "source-over";
+   drawCircle(ctx,x,y,oRadius,options);
+   ctx.globalCompositeOperation = "destination-out";
+   drawCircle(ctx,x,y,iRadius,options);
+   ctx.globalCompositeOperation = "source-over";
 }
 
 
 function drawPie(ctx,x,y,outerRadius,innerRadius,startangle,endangle,additive,options){
-    var startingAngle = degreesToRadians(startangle);
-    var arcSize = degreesToRadians(endangle);
-    var endingAngle = startingAngle + arcSize;
+   var startingAngle = degreesToRadians(startangle);
+   var arcSize = degreesToRadians(endangle);
+   var endingAngle = startingAngle + arcSize;
 
-    ctx.beginPath();
-    ctx.arc(x, y, outerRadius, startingAngle, endingAngle, !additive);
-    ctx.arc(x, y, innerRadius, endingAngle, startingAngle, additive);
-    ctx.closePath();
-    fillIt(ctx,options);
-    strokeIt(ctx,options);
+   ctx.beginPath();
+   ctx.arc(x, y, outerRadius, startingAngle, endingAngle, !additive);
+   ctx.arc(x, y, innerRadius, endingAngle, startingAngle, additive);
+   ctx.closePath();
+   fillIt(ctx,options);
+   strokeIt(ctx,options);
 }
+
+
+function drawPoints(ctx,line,radius,options) {
+   for(var i in line) {
+      drawCircle(ctx,line[i].x,line[i].y,radius,options);
+   }
+}
+function drawGrid(ctx,rows,cols,x,y,w,h,options) {
+   for(var i=0;i<=rows;i++) {
+      drawSegment(ctx,x,(h*(i/rows))+y,x+w,(h*(i/rows))+y,options)
+   }
+   for(var i=0;i<=cols;i++) {
+      drawSegment(ctx,(w*(i/cols))+x,y,(w*(i/cols))+x,y+h,options)
+   }
+}
+function drawLineGraph(ctx,line,x,y,w,h){
+   drawGrid(ctx,3,5,x,y,w,h,{
+      strokeStyle:"#ddd",
+      lineWidth:2,
+      lineJoin:"round",
+      lineCap:"round"
+   })
+   drawLine(ctx,line,{
+      strokeStyle:"black",
+      lineWidth:6,
+      lineJoin:"round",
+      lineCap:"round"
+   });
+   drawPoints(ctx,line,6,{
+      fillStyle:"white",
+      strokeStyle:"black",
+      lineWidth:4
+   })
+}
+
 
 
 
 function degreesToRadians(degrees) {
-    return (degrees * Math.PI)/180;
+   return (degrees * Math.PI)/180;
 }
 function getSatellite(start,angle,distance,isX) {
-    return start+Math[isX?"cos":"sin"](degreesToRadians(angle))*distance;
+   return start+Math[isX?"cos":"sin"](degreesToRadians(angle))*distance;
 }
 
 
@@ -106,16 +141,16 @@ function getSatellite(start,angle,distance,isX) {
 
 
 function strokeIt(ctx,options) {
-    if(!options) return;
-    if(options.lineWidth) {
-        ctx = overRide(ctx,options);
-        ctx.stroke();
-    }
+   if(!options) return;
+   if(options.lineWidth) {
+      ctx = overRide(ctx,options);
+      ctx.stroke();
+   }
 }
 function fillIt(ctx,options){
-    if(!options) return;
-    if(options.fillStyle) {
-        ctx = overRide(ctx,options);
-        ctx.fill();
-    }
+   if(!options) return;
+   if(options.fillStyle) {
+      ctx = overRide(ctx,options);
+      ctx.fill();
+   }
 }
