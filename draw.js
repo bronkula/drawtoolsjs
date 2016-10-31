f5cdcce0fda25185b03d0f9263b42ab38669c8e2
Hamilton <hamdiggy@gmail.com>
2016-10-31 01:21:48 -0700

    added drawroundrect

--- draw.js
+++ draw.js
@@ -136,6 +136,29 @@ function drawPie(ctx,x,y,outerRadius,innerRadius,startangle,endangle,additive,op
    strokeIt(ctx,options);
 }
 
+// http://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-on-html-canvas
+function drawRoundRect(ctx, x, y, width, height, radius, options) {
+  if (typeof radius === 'undefined') radius = 0;
+  if (typeof radius === 'number') {
+    radius = {tl: radius, tr: radius, br: radius, bl: radius};
+  } else {
+    radius = overRide({tl: 0, tr: 0, br: 0, bl: 0},radius);
+  }
+  ctx.beginPath();
+  ctx.moveTo(x + radius.tl, y);
+  ctx.lineTo(x + width - radius.tr, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+  ctx.lineTo(x + width, y + height - radius.br);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+  ctx.lineTo(x + radius.bl, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+  ctx.lineTo(x, y + radius.tl);
+  ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+  ctx.closePath();
+  strokeIt(ctx,options);
+  fillIt(ctx,options);
+}
+
 /* draw a series of circle at x y coordinates */
 function drawPoints(ctx,line,radius,options) {
    for(var i in line) {
