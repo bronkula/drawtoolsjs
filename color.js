aa2bf3374274567d3cc9f1bf40ba9181bb56af47
Hamilton <hamdiggy@gmail.com>
2016-11-27 21:44:42 -0800

    Update color.js

--- color.js
+++ color.js
@@ -1,176 +1,4 @@
 
-
-
-function rgbToHex(r,g,b) {
-    if(g===undefined) {
-        var g = r.g;
-        var b = r.b;
-        var r = r.r;
-    }
-    return ("0"+r.toString(16)).substr(-2)+
-        ("0"+g.toString(16)).substr(-2)+
-        ("0"+b.toString(16)).substr(-2);
-}
-function hexToRgb(h) {
-    return {r:parseInt(h.substr(0,2),16),
-        g:parseInt(h.substr(2,2),16),
-        b:parseInt(h.substr(4,2),16)};
-}
-
-function hslToRgb(h, s, l){
-    if(s===undefined) {
-        var s = h.s;
-        var l = h.l;
-        var h = h.h;
-    }
-    var r, g, b;
-
-    if(s == 0){
-        r = g = b = l;
-    }else{
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
-
-    return {r:r * 255, g:g * 255, b:b * 255};
-}
-function hue2rgb(p, q, t){
-    if(t < 0) t += 1;
-    if(t > 1) t -= 1;
-    if(t < 1/6) return p + (q - p) * 6 * t;
-    if(t < 1/2) return q;
-    if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-    return p;
-}
-
-function rgbToHsl(r, g, b){
-    if(g===undefined) {
-        var g = r.g;
-        var b = r.b;
-        var r = r.r;
-    }
-    r /= 255, g /= 255, b /= 255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
-
-    if(max == min){
-        h = s = 0; // achromatic
-    }else{
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
-    }
-
-    return {h:h*360, s:s*100, l:l*100};
-}
-
-function rgbToHsv(r, g, b) {
-    if(g===undefined) {
-        var g = r.g;
-        var b = r.b;
-        var r = r.r;
-    }
-    r = r/255, g = g/255, b = b/255;
-
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, v = max;
-
-    var d = max - min;
-    s = max == 0 ? 0 : d / max;
-
-    if (max == min) {
-        h = 0; // achromatic
-    } else {
-        switch (max) {
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-
-        h /= 6;
-    }
-
-    return {h:h, s:s, v:v};
-}
-
-function hsvToRgb(h, s, v) {
-    if(s===undefined) {
-        var s = h.s;
-        var v = h.v;
-        var h = h.h;
-    }
-    var r, g, b;
-
-    var i = Math.floor(h * 6);
-    var f = h * 6 - i;
-    var p = v * (1 - s);
-    var q = v * (1 - f * s);
-    var t = v * (1 - (1 - f) * s);
-
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-
-    return {r:r * 255, g:g * 255, b:b * 255 };
-}
-
-
-function cmykToRgb(c,m,y,k){
-    if(m===undefined) {
-        var m = c.m;
-        var y = c.y;
-        var k = c.k;
-        var c = c.c;
-    }
-    var r=0,g=0,b=0;
-
-    c = c / 100;
-    m = m / 100;
-    y = y / 100;
-    k = k / 100;
-
-    r = 1 - Math.min( 1, c * ( 1 - k ) + k );
-    g = 1 - Math.min( 1, m * ( 1 - k ) + k );
-    b = 1 - Math.min( 1, y * ( 1 - k ) + k );
-
-    return {r:r*255,g:g*255,b:b*255};
-}
-
-function rgbToCmyk(r,g,b){
-    if(g===undefined) {
-        var g = r.g;
-        var b = r.b;
-        var r = r.r;
-    }
-    var c=0,m=0,y=0,k=0;
-
-    r = r / 255;
-    g = g / 255;
-    b = b / 255;
-
-    k = Math.min( 1 - r, 1 - g, 1 - b );
-    c = ( 1 - r - k ) / ( 1 - k );
-    m = ( 1 - g - k ) / ( 1 - k );
-    y = ( 1 - b - k ) / ( 1 - k );
-
-    return {c:c*100,m:m*100,y:y*100,k:k*100};
-}
-
-
 function RGB(r,g,b) {
     this.r = +r;
     this.g = +g;
@@ -191,7 +19,7 @@ function COLOR(t,o) {
     this.rgb = new RGB(0,0,0);
     this.hsl = new HSL(0,0,0);
     this.cmyk = new CMYK(0,0,0,0);
-    this.hex = 0;
+    this.hex = "000000";
     if(t!==undefined) {
         this.ov(this[t],o);
         this.updateVals(t);
@@ -235,7 +63,7 @@ function COLOR(t,o) {
                 this.ov(this.cmyk,v);
                 break;
             case "hex":
-                this.x = v;
+                this.hex = v;
                 break;
         }
         this.updateVals(k);
@@ -259,27 +87,93 @@ function COLOR(t,o) {
         return this;
     }
     COLOR.prototype.rgbToHsl = function() {
-        this.ov(this.hsl,rgbToHsl(this.rgb));
+        var r=this.rgb.r / 255,
+            g=this.rgb.g / 255,
+            b=this.rgb.b / 255,
+            max = Math.max(r, g, b),
+            min = Math.min(r, g, b),
+            h, s, l = (max + min) / 2;
+        if(max == min){
+            h = s = 0; // achromatic
+        }else{
+            var d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+            switch(max){
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6;
+        }
+        this.hsl.h = h * 360;
+        this.hsl.s = s * 100;
+        this.hsl.l = l * 100;
         return this;
     };
     COLOR.prototype.rgbToCmyk = function() {
-        this.ov(this.cmyk,rgbToCmyk(this.rgb));
+        var r = this.rgb.r / 255,
+            g = this.rgb.g / 255,
+            b = this.rgb.b / 255,
+            k = Math.min( 1 - r, 1 - g, 1 - b ),
+            c = ( 1 - r - k ) / ( 1 - k ),
+            m = ( 1 - g - k ) / ( 1 - k ),
+            y = ( 1 - b - k ) / ( 1 - k );
+        this.cmyk.c = c * 255;
+        this.cmyk.m = m * 255;
+        this.cmyk.y = y * 255;
+        this.cmyk.k = k * 255;
         return this;
     };
     COLOR.prototype.rgbToHex = function() {
-        this.hex = rgbToHex(this.rgb);
+        this.hex = ("0"+Math.round(this.rgb.r).toString(16)).substr(-2)+
+        ("0"+Math.round(this.rgb.g).toString(16)).substr(-2)+
+        ("0"+Math.round(this.rgb.b).toString(16)).substr(-2);
         return this;
     };
     COLOR.prototype.hslToRgb = function() {
-        this.ov(this.rgb,hslToRgb(this.hsl));
+        var h=this.hsl.h / 360,
+            s=this.hsl.s / 100,
+            l=this.hsl.l / 100,
+            r, g, b;
+        if(s == 0){
+            r = g = b = l;
+        }else{
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = this.hue2rgb(p, q, h + 1/3);
+            g = this.hue2rgb(p, q, h);
+            b = this.hue2rgb(p, q, h - 1/3);
+        }
+        this.rgb.r = r * 255;
+        this.rgb.g = g * 255;
+        this.rgb.b = b * 255;
         return this;
     };
+    COLOR.prototype.hue2rgb = function(p, q, t){
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return p + (q - p) * 6 * t;
+        if(t < 1/2) return q;
+        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+    };
     COLOR.prototype.cmykToRgb = function() {
-        this.ov(this.rgb,cmykToRgb(this.cmyk));
+        var c = this.cmyk.c / 100,
+            m = this.cmyk.m / 100,
+            y = this.cmyk.y / 100,
+            k = this.cmyk.k / 100,
+            r = 1 - Math.min( 1, c * ( 1 - k ) + k ),
+            g = 1 - Math.min( 1, m * ( 1 - k ) + k ),
+            b = 1 - Math.min( 1, y * ( 1 - k ) + k );
+        this.rgb.r = r*255;
+        this.rgb.g = g*255;
+        this.rgb.b = b*255;
         return this;
     };
     COLOR.prototype.hexToRgb = function() {
-        this.ov(this.rgb,hexToRgb(this.hex));
+        this.rgb.r = parseInt(this.hex.substr(0,2),16);
+        this.rgb.g = parseInt(this.hex.substr(2,2),16);
+        this.rgb.b = parseInt(this.hex.substr(4,2),16);
         return this;
     };
     COLOR.prototype.toString = function(type) {
