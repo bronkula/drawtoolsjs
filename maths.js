c69b50a809cae41c64138ab1453437c176e351a4
Hamilton <hamdiggy@gmail.com>
2017-11-03 11:03:17 -0700

    added positional maths

--- maths.js
+++ maths.js
@@ -74,3 +74,70 @@ function rangeRatio(n,nmin,nmax,omin,omax) {
 function simpleRatio(nmin,nmax,omax) {
    return nmin/nmax*omax;
 }
+
+
+
+
+
+/*------------------------ Positional Functions ------------------------------------*/
+/* The distance between two points */
+function pointDistance(x1,y1,x2,y2) {
+    var dx = x1 - x2;
+    var dy = y1 - y2;
+    return distance = Math.sqrt(dx * dx + dy * dy);
+}
+/* Return a point between one point and another: Position1, Position2, Percentage */
+function positionToward(x1,y1,x2,y2,p) {
+  return {x:numberToward(x1,x2,p),y:numberToward(y1,y2,p)};
+}
+/* Expects an XY object, an angle, and a distance. Returns an XY object */
+function getSatelliteXY(pos,angle,distance) {
+   return {
+      x:getSatellite(pos.x,angle,distance,true),
+      y:getSatellite(pos.y,angle,distance,false)
+   };
+}
+/* check if two number ranges overlap */
+function overlap(a0,a1,b0,b1){
+    return Math.min(a0,a1) <= Math.max(b0,b1) && Math.min(b0,b1) <= Math.max(a0,a1);
+}
+/* check if two boxes overlap */
+function intersectBox(x0,y0,x1,y1,x2,y2,x3,y3) {
+    return overlap(x0,x1,x2,x3) && overlap(y0,y1,y2,y3);
+}
+/* determine which side of a line a point is on */
+function pointSide(px,py,x0,y0,x1,y1) {
+    return vxs(x1-x0,y1-y0,px-x0,py-y0);
+}
+/* Intersect: Calculate the point of intersection between two lines. */
+function intersect(x0,y0,x1,y1,x2,y2,x3,y3) {
+    return new xy(
+    vxs(vxs(x0,y0,x1,y1),x0-x1,vxs(x2,y2,x3,y3),x2-x3) /
+    vxs(x0-x1,y0-y1,x2-x3,y2-y3), 
+    vxs(vxs(x0,y0,x1,y1),y0-y1,vxs(x2,y2,x3,y3),y2-y3) /
+    vxs(x0-x1,y0-y1,x2-x3,y2-y3)
+    );
+}
+/* Calculate if two lines intersect */
+function isIntersect(x0,y0,x1,y1,x2,y2,x3,y3) {
+    return IntersectBox(x0,y0,x1,y1, x2,y2,x3,y3)
+        && abs(PointSide(x2,y2,x0,y0,x1,y1) + PointSide(x3,y3,x0,y0,x1,y1)) != 2
+        && abs(PointSide(x0,y0,x2,y2,x3,y3) + PointSide(x1,y1,x2,y2,x3,y3)) != 2;
+}
+/* Detect if a point is in a rectangle */
+function pointInRect(px,py,x1,y1,x2,y2){
+   return px >= x1 && px <= x2 && py >= y1 && py <= y2;
+}
+/* Detect if a rectangle is fully within another rectangle */
+function rectInRect(x0,y0,x1,y1,x2,y2,x3,y3) {
+   return pointInRect(x0,y0,x2,y2,x3,y3) && pointInRect(x1,y1,x2,y2,x3,y3);
+}
+/* detect if a circle is touching another circle. Use 0 for r1 if a point */
+function detectCircleCollission(x1,y1,r1,x2,y2,r2) {
+    var dx = x1 - x2;
+    var dy = y1 - y2;
+    var distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < r1 + r2) return true;
+    else return false;
+}
