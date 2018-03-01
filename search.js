55a2de34efc085e846780b81ce7de6bcc94109f3
Hamilton <hamdiggy@gmail.com>
2018-03-01 09:33:32 -0800

    updated function, added definitions, examples

--- search.js
+++ search.js
@@ -1,31 +1,57 @@
 // Expects jQuery and Lodash
 
-function showData(arr,template_string,target){
+
+/* 
+This function takes an array of objects, 
+stamps each object onto a lodash template string, 
+then places that output into a target_selector jQuery element
+or returns the output string if no target_selector is given
+
+example:
+showDataList(
+	[{x:10,y:10},{x:20,y:5}],
+	`<div><%= x %> x <%= y%></div>`,
+	'.output'
+	);
+*/
+function showDataList(object_array,template_string,target_selector){
 	var output = "";
 	var templ = _.template(template_string);
-	for(var i in arr) {
-		output += templ(arr[i]);
+	for(var i in object_array) {
+		output += templ(object_array[i]);
 	}
-	if(!target) return output;
-	else $(target).html(output);
+	if(!target_selector) return output;
+	else $(target_selector).html(output);
 }
 
-function searchData(data,search_str,props,template_string,target){
-	var prop_search = props.split(",");
-	// predicate
-	var arr = _.filter(data,function(obj){
 
+/*
+This function takes an array of objects,
+a search_string to search for in each object,
+a string of comma separated search_properties in each object to search in,
+an optional template_string and target_selector to activate the showDataList function
+
+example:
+showDataList(
+	[{x:10,y:10},{x:20,y:5}],
+	'George',
+	'email,name,description',
+	`<div><%= x %> x <%= y%></div>`,
+	'.output'
+	);
+*/
+function searchDataList(object_array,search_string,search_properties,template_string,target_selector){
+	var prop_search = search_properties.split(",");
+	var arr = _.filter(object_array,function(obj){
 		for(var i in prop_search) {
-			if((""+obj[prop_search[i]]).search(
-			RegExp(search_str,'i')
-			) != -1) return true;
+			return (""+obj[prop_search[i]]).search(RegExp(search_string,'i')) >= 0;
 		}
-
 		return false;
 	});
-	showData(
-		arr,
-		template_string,
-		target
-		)
+	if(template_string === undefined) return arr;
+	else if(target_selector === undefined) {
+		return showDataList(arr, template_string);
+	} else {
+		showDataList(arr, template_string, target_selector);
+	}
 }
