899f251bf97e5d95478da6207800c5dfc0e8e0dc
Hamilton <hamdiggy@gmail.com>
2018-12-11 15:19:51 -0800

    Removed jQuery dependence

--- events.js
+++ events.js
@@ -6,30 +6,18 @@
 */
 
 /*------------------------------- Point Detection Functions -----------------------*/
-/* These Require jQuery Events */
-/* Determine if an event is a touch event */
-function isTouch(e) {
-   return e.type.substring(0,5) == "touch";
-}
 /* Return an array of either touches or a click */
-function ev(e){
-   if(isTouch(e)) {
-      if(!e.originalEvent.touches.length) return e.originalEvent.changedTouches;
-      else return e.originalEvent.touches;
-   } else {
-      return [e];
-   }
-}
+const evPoints = e =>
+   e.type.substring(0,5)!="touch"?
+      [e]:
+      !e.originalEvent.touches.length?
+         e.originalEvent.changedTouches:
+         e.originalEvent.touches;
 /* return an offset xy object for the position of the click or touch in the object */
 /* pass in an optional object that will be used for basis */
-function getEXY(e,o) {
-   var offs = $(o||e.target).offset();
-   x = e.pageX - offs.left;
-   y = e.pageY - offs.top;
-   var ratio = ratio===undefined?1:ratio;
-   return {x:ratio*x,y:ratio*y};
-}
+const getEXY = (e,o) => ({
+   x:e.pageX-(o||e.target).offsetParent.offsetLeft,
+   y:e.pageY-(o||e.target).offsetParent.offsetTop
+});
 /* Return the first xy position from an event, whether touch or click */
-function getEventXY(e,o){
-   return getEXY(ev(e)[0],o);
-}
+const getEventXY = (e,o) => getEXY(evPoints(e)[0],o);
