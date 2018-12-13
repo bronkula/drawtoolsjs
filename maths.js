80d3e4fb422e0b4c0bb33e8dc69876f2b1ff8806
Hamilton <hamdiggy@gmail.com>
2018-12-12 22:40:15 -0800

    Update maths.js

--- maths.js
+++ maths.js
@@ -5,25 +5,6 @@
  * Website: hamiltondraws.com
 */
 
-/* EVENTS.JS */
-/*------------------------------- Point Detection Functions -----------------------*/
-/* Return an array of either touches or a click */
-const evPoints = e =>
-   e.type.substring(0,5)!="touch"?
-      [e]:
-      !e.originalEvent.touches.length?
-         e.originalEvent.changedTouches:
-         e.originalEvent.touches;
-/* return an offset xy object for the position of the click or touch in the object */
-/* pass in an optional object that will be used for basis */
-const getEXY = (e,o) => ({
-   x:e.pageX-(o||e.target).offsetLeft,
-   y:e.pageY-(o||e.target).offsetTop
-});
-/* Return the first xy position from an event, whether touch or click */
-const getEventXY = (e,o) => getEXY(evPoints(e)[0],o);
-
-
 
 /* MATHS.JS */
 /*---------------------- Math Helper Functions -----------------------------*/
