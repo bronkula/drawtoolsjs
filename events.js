84a0fbebfcdfbe3fb7839f7e94fec07fd13b719d
Hamilton <hamdiggy@gmail.com>
2018-12-14 18:52:28 -0800

    fixed the offsets

--- events.js
+++ events.js
@@ -5,19 +5,13 @@
  * Website: hamiltondraws.com
 */
 
+/* EVENTS.JS */
 /*------------------------------- Point Detection Functions -----------------------*/
 /* Return an array of either touches or a click */
 const evPoints = e =>
-   e.type.substring(0,5)!="touch"?
-      [e]:
-      !e.originalEvent.touches.length?
-         e.originalEvent.changedTouches:
-         e.originalEvent.touches;
+   e.type.substring(0,5)!="touch"?[e]:!e.originalEvent.touches.length?e.originalEvent.changedTouches:e.originalEvent.touches;
 /* return an offset xy object for the position of the click or touch in the object */
 /* pass in an optional object that will be used for basis */
-const getEXY = (e,o) => ({
-   x:e.pageX-(o||e.target).offsetParent.offsetLeft,
-   y:e.pageY-(o||e.target).offsetParent.offsetTop
-});
+const getEXY = (e,o) => { let rect = (o||e.target).getBoundingClientRect(); return ({ x:e.pageX-rect.left, y:e.pageY-rect.top }); }
 /* Return the first xy position from an event, whether touch or click */
 const getEventXY = (e,o) => getEXY(evPoints(e)[0],o);
